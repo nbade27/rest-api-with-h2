0f1e7aea4daa46adafa43bfdc74402c635dcd81e
nbade27 <nbade27@gmail.com>
2024-10-31 17:54:23 +0530

    added lombock and loggers

--- src/main/java/com/naga/rest_api/api/DemoController.java
+++ src/main/java/com/naga/rest_api/api/DemoController.java
@@ -5,13 +5,18 @@ import java.util.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class DemoController {
 
-    @GetMapping("/get-time")
-    public Map<String,Object> getDetails(){
+    @GetMapping("/time")
+    public Map<String,Object> getTimeMethod(){
+        log.info("Started executing getTimeMethod");
         Map<String, Object> map = new HashMap<>();
         map.put("time", new Date());
+        log.info("Exiting getTimeMethod");
         return map;
     }
 
